a68bbaf9bea6f4c949af4400bb45bbca131341e9
Cameron Lee <cameronkmlee10@gmail.com>
2025-12-04 10:57:35 -0500

    Add Express JSON parser middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
 app.use("/api/devices", devicesRouter);
 
 const server = http.createServer(app);
