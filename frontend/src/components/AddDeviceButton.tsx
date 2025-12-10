62d7b53e4d56bf5cdbc56209b3ca0aba46a4dedd
Cameron Lee <cameronkmlee10@gmail.com>
2025-12-09 21:56:52 -0500

    Add basic non-functional AddDeviceButton

---
+++ frontend/src/components/AddDeviceButton.tsx
@@ -0,0 +1,7 @@
+export default function AddDeviceButton() {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-400 cursor-pointer rounded-lg px-3 py-2">
+      Add Device
+    </button>
+  );
+}
