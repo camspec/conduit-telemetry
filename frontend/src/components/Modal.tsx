7bfc640f9d547d6d8c6eeda11129fbcc318364e5
Cameron Lee <cameronkmlee10@gmail.com>
2025-12-11 23:45:28 -0500

    Create modal portal

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,8 +1,15 @@
+import { createPortal } from "react-dom";
+
 type ModalProps = {
   onClose: () => void;
   content: React.ReactNode;
 };
 
 export default function Modal({ onClose, content }: ModalProps) {
-  
+  return createPortal(
+    <div>
+      <div>{content}</div>
+    </div>,
+    document.body,
+  );
 }
