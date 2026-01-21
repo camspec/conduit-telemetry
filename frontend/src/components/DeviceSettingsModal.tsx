84e8e4519b2eb4e3c86aadd0c3acb29b5c704905
Cameron Lee <cameronkmlee10@gmail.com>
2026-01-20 22:40:11 -0500

    Add non-functional fields to device settings modal

--- frontend/src/components/DeviceSettingsModal.tsx
+++ frontend/src/components/DeviceSettingsModal.tsx
@@ -14,7 +14,37 @@ export default function DeviceSettingsModal({
   return (
     <Modal onClose={onClose}>
       <h2 className="font-bold text-xl mb-6">Edit {device.name}</h2>
-      <button>Regenerate API key</button>
+      <form className="space-y-4">
+        <div>
+          <label htmlFor="name" className="block text-sm mb-1 text-gray-300">
+            Device name
+          </label>
+          <input
+            id="name"
+            type="text"
+            className="w-full px-2 py-1 bg-slate-900 border border-slate-600 rounded-lg"
+            required
+          ></input>
+        </div>
+
+        <div>
+          <label
+            htmlFor="category"
+            className="block text-sm mb-1 text-gray-300"
+          >
+            Category
+          </label>
+          <input
+            id="category"
+            type="text"
+            className="w-full px-2 py-1 bg-slate-900 border border-slate-600 rounded-lg"
+            required
+          ></input>
+        </div>
+      </form>
+      <button className="px-3 py-2 rounded-xl bg-slate-700 hover:bg-slate-600 cursor-pointer text-sm my-5">
+        Regenerate API key
+      </button>
       <div className="flex justify-between">
         <DeleteDeviceButton device={device} />
         <div className="flex gap-3">
